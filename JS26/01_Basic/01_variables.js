02ef2e8c4394514f599321a87c1b3a866c461dda
GLOW-SHINE <kayalpalash1911@gmail.com>
2026-01-10 23:34:07 +0530

    100126  01_variables

---
+++ JS26/01_Basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 1234567891
+let accountEmail = "kayalpalash1911@gmail.com"
+var accountPassword = "12345"
+
+// this is possiable to create a variable ,
+accountCity = "WEST BENGAL"
+
+ // "const" value do not possiable to changes
+// accountID = 123
+console.log(accountID)
+
+/*
+
+        Prefer not to use "var" for variable , 
+        because of issues in block scope and functional scope
+
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
