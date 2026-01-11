f5e05956057071d84b5e46222eadf3535b9b0640
GLOW-SHINE <kayalpalash1911@gmail.com>
2026-01-12 00:44:17 +0530

    120126 (02_Datatypes)

---
+++ JS26/01_Basic/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version 
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 3 )
+
+
+console.log("Palash")
+
+// install "Prettier" EXTENSIONS 
+
+let name = "palash"
+let age  = 35
+let isLoggedIn = false
+
+
+// number => 2 to power 
+// bigint 
+// string => ""
+// boolean => true/false
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
