3f79e250ed97b29b6c198822b77c6e26ce65f02d
GLOW-SHINE <kayalpalash1911@gmail.com>
2026-01-15 21:24:42 +0530

    15012026  datatypes

---
+++ JS26/01_Basic/03_conversionOperations.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log( ( 3 + 4 ) * 5 % 3);
+console.log();
+console.log();
+
+
---
+++ JS26/01_Basic/datatype-summery.js
@@ -0,0 +1,20 @@
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+// Reference (Non Primitive)
+
+//  Array , Objects , Functions
+
+const heros = ["Shaktiman", "Naagraj", "doga"];
+let myObj = {
+    name : "Palash",
+    age : 32,
+}
+
+
